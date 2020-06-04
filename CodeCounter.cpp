25629a64ffb83ead1ae7d055f052bf859dc652fa
ghb123 <admin@guanhaobo.cn>
2020-06-05 02:20:52 +0800

    修复了已知bug

--- CodeCounter.cpp
+++ CodeCounter.cpp
@@ -156,9 +156,6 @@ void run2(int argc, char **argv)
 /* Ö÷º¯Êý */
 int main(int argc, char **argv)
 {
-    for (int i = 0; i < argc; i++)
-        cout << argv[i] << endl;
-    return 0;
     if (argc < 3)
     {
         cout << "error" << endl;
