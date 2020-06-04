38a40af70af1610007e74dc6fa65706fb1964fd0
ghb123 <admin@guanhaobo.cn>
2020-06-05 00:17:29 +0800

    增加命令行参数

--- CodeCounter.cpp
+++ CodeCounter.cpp
@@ -1,26 +1,28 @@
-//www.guanhaobo.cn
+/*************************************************
+*   作者：关浩博
+*   日期：2020-05-04
+*   描述：代码行数查看器
+*   链接：https://github.com/ghb123/CodeCounter
+**************************************************/
+#include <bits/stdc++.h>
 #include <io.h>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <algorithm>
 using namespace std;
-vector<string> files; //存储文件路径
-vector<string> type;  //指定扩展名
+/* 存储文件路径 */
+vector<string> files;
 
-//获取路径path下所有的文件名（包括子文件夹里的），存储在files中
+/* 指定扩展名 */
+vector<string> type;
+
+/* 功能获取路径path下所有的文件路径（包括子文件夹里的），存储在files中 */
 void getFiles(string path, vector<string> &files)
 {
-    //文件句柄
     long hFile = 0;
-    //文件信息
     struct _finddata_t fileinfo;
     string p;
     if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1)
     {
         do
-        { //如果是目录,迭代之
-            //如果不是,加入列表
+        {
             if ((fileinfo.attrib & _A_SUBDIR))
             {
                 if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
@@ -33,7 +35,7 @@ void getFiles(string path, vector<string> &files)
     }
 }
 
-//获取文件行数
+/* 获取文件行数 */
 int getLineNum(string path)
 {
     int ans = 0, first = 1;
@@ -50,24 +52,30 @@ int getLineNum(string path)
     return ans + 1;
 }
 
-//判断是否是目标扩展名
+/* 判断是否是目标扩展名 */
 bool isTarget(string path)
 {
     if (type.size() == 0)
         return true;
     string name = path.substr(path.find_last_of(".") + 1);
+    transform(name.begin(), name.end(), name.begin(), ::tolower);
     if (find(type.begin(), type.end(), name) != type.end())
         return true;
     return false;
 }
 
-int main()
+/* 主函数 */
+int main(int argc, char **argv)
 {
-    string filePath;
-    cout << "请输入项目文件夹的路径：";
-    cin >> filePath;
+    if (argc < 3)
+    {
+        cout << "error" << endl;
+        return -1;
+    }
+    string filePath = argv[1];
     //设置扩展名
-    type.push_back("java");
+    for (int i = 2; i < argc; i++)
+        type.push_back(argv[i]);
     //读取所有文件的路径
     getFiles(filePath, files);
     int num, ans = 0;
@@ -78,7 +86,7 @@ int main()
         {
             num = getLineNum(files[i]);
             ans += num;
-            cout << files[i] << "    行数：" << num << endl;
+            cout << files[i] << "\t行数：" << num << endl;
         }
     }
     cout << endl
